d2809cf1a380ee364fd13ae4052285a8910b23e9
gbernsteiner <guetar@gmx.at>
2012-11-22 15:20:36 +0100

    Changed Bediener

--- Bediener.java
+++ Bediener.java
@@ -5,6 +5,10 @@
  */
 public abstract class Bediener extends Android {
     
+    public Bediener(int snr, Skin skin) {
+        super(snr, skin);
+    }
+    
     public String validateSkin(Skin s) {
         return s.fromBediener(this);
     }
