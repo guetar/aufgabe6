7c87e41a8c2e88fa50fe406561e69c842f066bcb
Matthias <Matthias@128.131.209.198>
2012-11-22 15:20:09 +0100

    SkinGepanzert

---
+++ SkinGepanzert.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matthias
+ */
+public class SkinGepanzert extends Skin{
+    //VB: b!=null
+    //NB: return "" wenn Skin mit b kompatibel ist
+    public String fromBeschuetzer(Beschuetzer b){
+    return "";
+    } 
+}
