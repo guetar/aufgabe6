1edaa80a8e771751f8d06f09f0d37ca5f9a7e91d
Matthias <Matthias@128.131.209.198>
2012-11-22 15:05:22 +0100

    Skin und SkinBeruehrungssensitiv

---
+++ Skin.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author Matthias
+ */
+public abstract class Skin {
+    
+    //VB: s!=null
+    //NB: return "" wenn Skin mit s kompatibel ist
+    public String fromSchwerarbeiter(Schwerarbeiter s){
+    return null;
+    }
+    
+    //VB: b!=null
+    //NB: return "" wenn Skin mit b kompatibel ist
+    public String fromBediener(Bediener b){
+    return null;
+    }
+  
+    //VB: b!=null
+    //NB: return "" wenn Skin mit b kompatibel ist
+    public String fromBediener(Beschuetzer b){
+    return null;
+    }  
+}
---
+++ SkinBeruehrungssensitiv.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author Matthias
+ */
+public class SkinBeruehrungssensitiv extends Skin{
+    
+    //VB: b!=null
+    //NB: return "" wenn Skin mit b kompatibel ist
+    public String fromBediener(){
+    return "";
+    }
+    
+}
