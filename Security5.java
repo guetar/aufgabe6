e430b8548a136474fae5478c3d68d42226a904a2
gbernsteiner <guetar@gmx.at>
2012-11-26 23:22:46 +0100

    Finished Software

--- Security5.java
+++ Security5.java
@@ -6,7 +6,8 @@
 public class Security5 extends Security45 {
     
     @Override
+    //NB: return null wenn Software vom Kaempfer verwendet wird
     public String fromKaempfer() {
-        return "";
+        return null;
     }
 }
